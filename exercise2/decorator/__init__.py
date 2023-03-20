cb28c3b6c36c172a6d9fc121bb40dae6cba1f67f
rdabek <rafal.dabek.com@gmail.com>
2023-03-21 00:56:44 +0100

    corrected some mistakes cause python has no clue on its own that there is a mistake, does it?

--- exercise2/decorator/__init__.py
+++ exercise2/decorator/__init__.py
@@ -1,5 +1,5 @@
-def dec(ev1: float, ev2: float, sign: chr, func: callable) -> callable:
-    def addition(ev1: float, ev2: float, func: callable) -> callable:
+def dec(ev1: float, ev2: float, sign: chr, func: callable) -> float:
+    def addition(ev1: float, ev2: float, func: callable) -> float:
         return func(ev1+ev2)
     
     if sign is not '+' and sign is not '-':
@@ -9,17 +9,13 @@ def dec(ev1: float, ev2: float, sign: chr, func: callable) -> callable:
     else:
         return addition(ev1, -ev2, func)
 
-def reversecomp(l: list[callable], x: float) -> callable:
-    def compose(f: callable(float), g: callable(float)) -> callable:
+def reversecomp(l: list[callable], x: float) -> callable(float):
+    def compose(f: callable(float), g: callable(float)) -> callable(float):
         return lambda x : f(g(x))
     
     lenList = len(l)
     retFunc = lambda x : l[lenList-1](x)
     for i in reversed(range(lenList - 1)):
-        #print("l+1: ")
-        #print(l[i+1])
-        #print("l: ")
-        #print(l[i])
         retFunc = compose(retFunc, l[i])
     
     return retFunc
